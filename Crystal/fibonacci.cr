a7ce6f24115b8204bd0215040a0761bc024ed46e
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-30 09:47:22 -0300

    Update fibonacci.cr

---
+++ Crystal/fibonacci.cr
@@ -0,0 +1,13 @@
+def fibonacci(x : Int32)
+  if x == 1 || x == 2
+  	return(1)
+  else
+  	return(fibonacci(x-1) + fibonacci(x-2))
+  end
+end
+
+x = 1
+while x < 10
+	puts(fibonacci(x))
+	x += 1
+end
