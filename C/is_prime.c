be742f9d63a767d0f52c7fe89fd63b11021e490f
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-30 10:50:44 -0300

    Update is_prime.c

--- C/is_prime.c
+++ C/is_prime.c
@@ -3,30 +3,30 @@
 
 int isPrime(int x)
 {
-	if (x == 2)
-		return 1;
-	else if (x % 2 == 0 || x < 2)
-		return 0;
-	else
-	{
-		for (int i = 3; i <= (int)sqrt((double)x); i++)
-			if (x % i == 0)
-				return 0;
+    if (x == 2)
+        return 1;
+    else if (x % 2 == 0 || x < 2)
+        return 0;
+    else
+    {
+        for (int i = 3; i <= (int)sqrt((double)x); i++)
+            if (x % i == 0)
+                return 0;
 
-		return 1;
-	}
+        return 1;
+    }
 }
 
 int main()
 {
-	int x;
+    int x;
 
-	scanf("%d", &x);
+    scanf("%d", &x);
 
-	if (isPrime(x))
-		printf("Yes\n");
-	else
-		printf("No\n");
+    if (isPrime(x))
+        printf("Yes\n");
+    else
+        printf("No\n");
 
-	return 0;
+    return 0;
 }
