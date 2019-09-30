ca21079ffc51505ee24c02694e0707be741785cd
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-30 13:51:26 -0300

    Create is_prime.lua

---
+++ Lua/is_prime.lua
@@ -0,0 +1,23 @@
+function isPrime(x)
+  if x == 2 then
+    return true
+  elseif x % 2 == 0 or x < 2 then
+    return false
+  else
+    for i=3,math.sqrt(x) do
+      if x % i == 0 then
+        return false
+      end
+    end
+    return true
+  end
+end
+
+local x
+x = tonumber(io.read())
+
+if isPrime(x) then
+  print("Yes")
+else
+  print("No")
+end
