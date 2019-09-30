0b61c9a017b6e5480fa97e519bc36186d4070296
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-30 10:48:52 -0300

    Create is_prime.c

---
+++ C/is_prime.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int x)
+{
+	if (x == 2)
+		return 1;
+	else if (x % 2 == 0 || x < 2)
+		return 0;
+	else
+	{
+		for (int i = 3; i <= (int)sqrt((double)x); i++)
+			if (x % i == 0)
+				return 0;
+
+		return 1;
+	}
+}
+
+int main()
+{
+	int x;
+
+	scanf("%d", &x);
+
+	if (isPrime(x))
+		printf("Yes\n");
+	else
+		printf("No\n");
+
+	return 0;
+}
