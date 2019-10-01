89d0415dddf1cb9df8131acff8564e8900180c41
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-10-01 10:50:30 -0300

    Create fibonacci.d

---
+++ D/fibonacci.d
@@ -0,0 +1,18 @@
+import std.stdio;
+import std.range;
+
+int fib(int x)
+{
+ 	if (x == 1 || x ==2)
+    	return(1);
+    else
+        return(fib(x-1) + fib(x-2));
+}
+
+void main()
+{
+    foreach(i; 1..10)
+    {
+    	writeln(fib(i));
+    }
+}
