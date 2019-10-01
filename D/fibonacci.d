747821ebf049d340597da78fd553d0a94b3905b8
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-10-01 10:51:33 -0300

    Update fibonacci.d

--- D/fibonacci.d
+++ D/fibonacci.d
@@ -3,8 +3,8 @@ import std.range;
 
 int fib(int x)
 {
- 	if (x == 1 || x ==2)
-    	return(1);
+    if (x == 1 || x ==2)
+        return(1);
     else
         return(fib(x-1) + fib(x-2));
 }
@@ -13,6 +13,6 @@ void main()
 {
     foreach(i; 1..10)
     {
-    	writeln(fib(i));
+        writeln(fib(i));
     }
 }
