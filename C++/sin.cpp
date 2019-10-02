b0fb2814dcf664b1d4e90c5ddb0de182d2658e9c
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-10-02 11:23:08 -0300

    Create sin.cpp

---
+++ C++/sin.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+#define PI 3.14159265359
+
+typedef long long unsigned int ll_uint;
+
+ll_uint factorial(ll_uint x)
+{
+    ll_uint f = 1;
+    for (ll_uint i = x; i > 1; i--)
+        f *= i;
+    return(f);
+}
+
+long double Sin(const long double angle, ll_uint precision)
+{
+    long double result = angle;
+    for (ll_uint i = 3, j = 0; j < precision; i += 2, j++)
+    {
+        if (j % 2 != 0)
+        {
+            result += pow(angle, i) / factorial(i);
+        }
+        else
+        {
+            result -= pow(angle, i) / factorial(i);
+        }
+    }
+    return(result);
+}
+
+int main()
+{
+    cout << setprecision(3) << fixed << Sin(PI/2, 10) << endl;
+    return 0;
+}
