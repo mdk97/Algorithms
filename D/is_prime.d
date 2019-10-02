ec4880c43b6be8000d08ce0b3135037df3a98eee
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-10-02 08:10:14 -0300

    Create is_prime.d

---
+++ D/is_prime.d
@@ -0,0 +1,31 @@
+import std.stdio;
+import std.range;
+import std.math;
+import std.conv;
+import std.string;
+
+bool isPrime(int x)
+{
+	if (x == 2)
+        return(true);
+    else if (x % 2 == 0 || x < 2)
+        return(false);
+    else
+    {
+    	foreach(i; 3..sqrt(cast(float)x) + 1)
+        {
+        	if (x % i == 0)
+                return(false);
+        }
+        return(true);
+    }
+}
+
+void main()
+{
+	string line = stdin.readln();
+    if (isPrime(to!int(chomp(line))))
+        writeln("Yes");
+    else
+        writeln("No");
+}
